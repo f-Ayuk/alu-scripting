24dc5920f4937c96db3c80b14891489d7ab3e233
f-Ayuk <f.ayuk@alustudent.com>
2023-09-10 14:50:31 -0700

    regular expression 2

---
+++ regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+# Ruby script that accepts one argument and passes it to a regular expression matching method
+# The regular expression must match hbt{2,5}n
+
+# Get the argument from the command line
+argument = ARGV[0]
+
+# Define the regular expression pattern
+pattern = /hbt{2,5}n/
+
+# Scan method to find all occurrences of the pattern in the argument
+matches = argument.scan(pattern)
+
+# Loop to print each match on a new line
+matches.each do |match|
+  puts match
+end
