01400044caccc87364ea63f73c9ae94595c512ed
f-Ayuk <f.ayuk@alustudent.com>
2023-09-13 11:06:55 -0700

    regular expressions

--- regular_expressions/3-repetition_token_2.rb
+++ regular_expressions/3-repetition_token_2.rb
@@ -6,7 +6,7 @@
 argument = ARGV[0]
 
 # Define the regular expression pattern
-pattern = /hbt+n/
+pattern = /hbt?n/
 
 # Use the scan method to find all occurrences of the pattern in the argument
 matches = argument.scan(pattern)
