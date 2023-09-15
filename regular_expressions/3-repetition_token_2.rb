e4cf05c6b54eae959ee3e93fb85464935a62177f
f-Ayuk <f.ayuk@alustudent.com>
2023-09-15 04:36:16 -0700

    regular expressions

--- regular_expressions/3-repetition_token_2.rb
+++ regular_expressions/3-repetition_token_2.rb
@@ -2,20 +2,20 @@
 # This is a Ruby script that accepts one argument and passes it to a regular expression matching method
 # The regular expression must match
 
-def match_regex(text)
-  regex = /hbt[nt]{0,5}/
-  match = text.match(regex)
-
-  if match.nil?
-    puts "No match found"
-  else
-    puts match.to_s
-  end
+# Check if an argument was provided
+if ARGV.empty?
+  puts "Usage: ./3-repetition_token_2.rb <input_string>"
+  exit(1)
 end
 
-if ARGV.length != 1
-  puts "Please provide one argument."
-else
-  text = ARGV[0]
-  match_regex(text)
-end
+# Get the input string from the command line argument
+input_string = ARGV[0]
+
+# Define the regular expression pattern
+pattern = /hbt+n|hb?tn/
+
+# Use the regular expression to find matches in the input string
+matches = input_string.scan(pattern)
+
+# Join and print the matched text
+puts matches.join
