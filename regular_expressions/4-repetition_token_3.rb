f7a7e5bdcf0e0cc141150c0d0333f06b4b37897a
f-Ayuk <f.ayuk@alustudent.com>
2023-09-15 04:38:46 -0700

    regular expressions

---
+++ regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+# Check if an argument was provided
+if ARGV.empty?
+  puts "Usage: ./script_name.rb <input_string>"
+  exit(1)
+end
+
+# Get the input string from the command line argument
+input_string = ARGV[0]
+
+# Use the regular expression pattern to find matches
+matches = input_string.scan(/hbt*n/)
+
+# Join and print the matched text
+puts matches.join
