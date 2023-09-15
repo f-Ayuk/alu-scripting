49dea7104dece902281bf3f29de88d5dcc4f78a7
f-Ayuk <f.ayuk@alustudent.com>
2023-09-15 05:12:58 -0700

    regular expressions

---
+++ regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+#regular expression matching method
+# Get the argument from the command line
+argument = ARGV[0]
+
+# Define the regular expression pattern
+pattern = /h.n/
+
+# Use the match method to check if the argument matches the pattern
+match = argument.match(pattern)
+
+# Print the result
+if match
+  puts match[0]
+else
+  puts "No match found"
+end
